b43d8c1fdcc8dec35c7b7a2568a29c4b2c20c5f1
YordanPopov <jordanpopov190@gmail.com>
2025-05-01 16:37:18 +0300

    remove and sort usings

--- ColorNoteTests/ColorNoteTests/Drivers/DriverFactory.cs
+++ ColorNoteTests/ColorNoteTests/Drivers/DriverFactory.cs
@@ -2,11 +2,6 @@
 using OpenQA.Selenium.Appium;
 using OpenQA.Selenium.Appium.Android;
 using OpenQA.Selenium.Appium.Service;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace ColorNoteTests.Drivers
 {
--- ColorNoteTests/ColorNoteTests/Tests/BaseTest.cs
+++ ColorNoteTests/ColorNoteTests/Tests/BaseTest.cs
@@ -1,10 +1,5 @@
 ﻿using ColorNoteTests.Drivers;
 using OpenQA.Selenium.Appium.Android;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace ColorNoteTests.Tests
 {
