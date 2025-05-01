46e4b748588d29998191fd167c07f59c99f1e7f0
YordanPopov <jordanpopov190@gmail.com>
2025-05-01 16:34:47 +0300

    initial commit

---
+++ ColorNoteTests/ColorNoteTests/Drivers/DriverFactory.cs
@@ -0,0 +1,48 @@
+﻿using ColorNoteTests.Config;
+using OpenQA.Selenium.Appium;
+using OpenQA.Selenium.Appium.Android;
+using OpenQA.Selenium.Appium.Service;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ColorNoteTests.Drivers
+{
+	public static class DriverFactory
+	{
+		private static AppiumLocalService _service;
+
+		public static AndroidDriver CreateAndroidDriver()
+		{
+			var config = ConfigReader.GetConfig();
+
+			_service = new AppiumServiceBuilder()
+				.WithIPAddress("127.0.0.1")
+				.UsingPort(4723)
+				.Build();
+			_service.Start();
+
+			var options = new AppiumOptions()
+			{
+				AutomationName = config.automationName.ToString(),
+				PlatformName = config.platformName.ToString(),
+				PlatformVersion = config.platformVersion.ToString(),
+				DeviceName = config.deviceName.ToString(),
+				App = config.app.ToString()
+			};
+			options.AddAdditionalAppiumOption("autoGrantPermissions", true);
+
+			return new AndroidDriver(_service.ServiceUrl ,options);
+		}
+
+		public static void StopService()
+		{
+			if (_service != null && _service.IsRunning)
+			{
+				_service.Dispose();
+			}
+		}
+	}
+}
---
+++ ColorNoteTests/ColorNoteTests/Tests/BaseTest.cs
@@ -0,0 +1,29 @@
+﻿using ColorNoteTests.Drivers;
+using OpenQA.Selenium.Appium.Android;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ColorNoteTests.Tests
+{
+	public class BaseTest
+	{
+		protected AndroidDriver _driver;
+
+		[OneTimeSetUp]
+		public void OneTimeSetUp()
+		{
+			_driver = DriverFactory.CreateAndroidDriver();
+		}
+
+		[OneTimeTearDown]
+		public void OneTimeTearDown()
+		{
+			_driver.Quit();
+			_driver.Dispose();
+			DriverFactory.StopService();
+		}
+	}
+}
