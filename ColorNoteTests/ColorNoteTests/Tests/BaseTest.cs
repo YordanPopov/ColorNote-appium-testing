a1fabcb6b860329edb831bcc5060ae594ce63387
YordanPopov <jordanpopov190@gmail.com>
2025-05-02 22:30:42 +0300

    modify

--- ColorNoteTests/ColorNoteTests/Tests/BaseTest.cs
+++ ColorNoteTests/ColorNoteTests/Tests/BaseTest.cs
@@ -1,4 +1,6 @@
 ﻿using ColorNoteTests.Drivers;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Appium;
 using OpenQA.Selenium.Appium.Android;
 
 namespace ColorNoteTests.Tests
@@ -11,6 +13,16 @@ namespace ColorNoteTests.Tests
 		public void OneTimeSetUp()
 		{
 			_driver = DriverFactory.CreateAndroidDriver();
+
+			try
+			{
+				var skipButton = _driver.FindElement(MobileBy.Id("com.socialnmobile.dictapps.notepad.color.note:id/btn_start_skip"));
+				skipButton.Click();
+			}
+			catch (NoSuchElementException)
+			{
+				Assert.Pass("Skip button not found!");
+			}
 		}
 
 		[OneTimeTearDown]
