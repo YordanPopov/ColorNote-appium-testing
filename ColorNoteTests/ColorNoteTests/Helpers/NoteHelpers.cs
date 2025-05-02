76b50147b04d1796ac39fabd829bfe8ac42282cc
YordanPopov <jordanpopov190@gmail.com>
2025-05-02 22:31:12 +0300

    Add Helpers methods

---
+++ ColorNoteTests/ColorNoteTests/Helpers/NoteHelpers.cs
@@ -0,0 +1,20 @@
+﻿using Bogus;
+
+namespace ColorNoteTests.Helpers
+{
+	public static class NoteHelpers
+	{
+		private static readonly Random _random = new Random();
+		private static readonly Faker _faker = new Faker();
+
+		public static string GenerateRandomTitle()
+		{
+			return "TITLE-" + _random.Next(1000, 9999).ToString();
+		}
+
+		public static string GenerateRandomContent()
+		{
+			return _faker.Lorem.Sentences(2, " ");
+		}
+	}
+}
