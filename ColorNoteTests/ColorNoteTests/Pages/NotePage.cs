bc064a05278725d80fbf008d0608a29ad06dc309
YordanPopov <jordanpopov190@gmail.com>
2025-05-06 16:26:25 +0300

    Add some locators and methods

--- ColorNoteTests/ColorNoteTests/Pages/NotePage.cs
+++ ColorNoteTests/ColorNoteTests/Pages/NotePage.cs
@@ -11,11 +11,24 @@ namespace ColorNoteTests.Pages
 		public NotePage(AndroidDriver driver) : base(driver) { }
 
 		private By titleField = MobileBy.Id("com.socialnmobile.dictapps.notepad.color.note:id/edit_title");
+
 		private By textField = MobileBy.Id("com.socialnmobile.dictapps.notepad.color.note:id/edit_note");
+
 		private By doneButton = MobileBy.Id("com.socialnmobile.dictapps.notepad.color.note:id/back_btn");
+
 		private By noteLocator = MobileBy.Id("com.socialnmobile.dictapps.notepad.color.note:id/title");
 
-		public ReadOnlyCollection<IWebElement> Notes => _wait.Until(ExpectedConditions.VisibilityOfAllElementsLocatedBy(MobileBy.Id("com.socialnmobile.dictapps.notepad.color.note:id/title")));
+		private By EditButton = MobileBy.Id("com.socialnmobile.dictapps.notepad.color.note:id/edit_btn");
+
+		private By menuButton = MobileBy.Id("com.socialnmobile.dictapps.notepad.color.note:id/menu_btn");
+
+		private By deleteButton = MobileBy.AndroidUIAutomator("new UiSelector().text(\"Delete\")");
+
+		private By OkayConfirmButton = MobileBy.Id("android:id/button1");
+
+		public ReadOnlyCollection<AppiumElement> Notes => _driver.FindElements(MobileBy.Id("com.socialnmobile.dictapps.notepad.color.note:id/title"));
+
+		public AppiumElement ViewNote => _driver.FindElement(MobileBy.Id("com.socialnmobile.dictapps.notepad.color.note:id/view_note"));
 
 		public void CreateNote(string title, string content)
 		{
@@ -23,6 +36,24 @@ namespace ColorNoteTests.Pages
 			EnterText(textField, content);
 			ClickElement(doneButton);
 			ClickElement(doneButton);
+		}
+
+		public void EditNote(string newTitle, string newContent)
+		{
+			ClickElement(EditButton);
+
+			EnterText(titleField, newTitle);
+			EnterText(textField, newContent);
+
+			ClickElement(doneButton);
+			ClickElement(doneButton);
+		}
+
+		public void DeleteNote()
+		{
+			ClickElement(menuButton);
+			ClickElement(deleteButton);
+			ClickElement(OkayConfirmButton);
 		}
 
 		public ReadOnlyCollection<IWebElement> WaitForLastCreatedNote()
