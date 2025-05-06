9e592614cf16a2b36049e8dc45922736aed22e80
YordanPopov <jordanpopov190@gmail.com>
2025-05-06 16:25:59 +0300

    nothing to commit

--- ColorNoteTests/ColorNoteTests/Pages/MainPage.cs
+++ ColorNoteTests/ColorNoteTests/Pages/MainPage.cs
@@ -11,7 +11,9 @@ namespace ColorNoteTests.Pages
 		public MainPage(AndroidDriver driver) : base(driver) { }
 
 		private By addButton = MobileBy.Id("com.socialnmobile.dictapps.notepad.color.note:id/main_btn1");
+
 		private By noteButton = MobileBy.AndroidUIAutomator("new UiSelector().text(\"Text\")");
+
 		private By checklistButton = MobileBy.AndroidUIAutomator("new UiSelector().text(\"Checklist\")");
 
 		public void OpenNote()
