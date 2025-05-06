239f7e535946455e2064e9291b9af0558827c74a
YordanPopov <jordanpopov190@gmail.com>
2025-05-06 16:27:11 +0300

    Add tests for editing and deleting note

--- ColorNoteTests/ColorNoteTests/Tests/NoteTests.cs
+++ ColorNoteTests/ColorNoteTests/Tests/NoteTests.cs
@@ -9,13 +9,13 @@ namespace ColorNoteTests.Tests
 		private MainPage _mainPage;
 		private NotePage _notePage;
 
-		[SetUp]
-		public void SetUp()
+		[OneTimeSetUp]
+		public void PageInitialization()
 		{
 			_mainPage = new MainPage(_driver);
 			_notePage = new NotePage(_driver);
 		}
-		[Test]
+		[Test, Order(1)]
 		public void CreateNoteSuccessfully()
 		{
 			string noteTitle = NoteHelpers.GenerateRandomTitle();
@@ -31,6 +31,41 @@ namespace ColorNoteTests.Tests
 				Assert.That(notes, Is.Not.Empty, "No notes found!");
 				Assert.That(notes.Last().Text, Is.EqualTo(noteTitle));
 			});
+		}
+
+		[Test, Order(2)]
+		public void EditNoteSuccessfully()
+		{
+			string editedTitle = "EDITED-" + NoteHelpers.GenerateRandomTitle();
+			string editedContent = "EDITED CONTENT";
+
+			_notePage.WaitForLastCreatedNote()
+			   .Last()
+			   .Click();
+
+			_notePage.EditNote(editedTitle, editedContent);
+
+			var editedNote = _notePage.WaitForLastCreatedNote().Last();
+
+			Assert.Multiple(() =>
+			{
+				Assert.That(editedNote.Text, Is.EqualTo(editedTitle));
+
+				editedNote.Click();
+
+				var noteContent = _notePage.ViewNote.Text;
+				Assert.That(noteContent, Is.EqualTo(editedContent));
+			});
+		}
+
+		[Test, Order(3)]
+		public void DeleteNoteSuccessfully()
+		{
+			_notePage.DeleteNote();
+
+			var notesAfterDeletion = _notePage.Notes;
+
+			Assert.That(notesAfterDeletion, Is.Empty);
 		}
 	}
 }
